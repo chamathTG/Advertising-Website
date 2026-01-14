1384beffe987fdfa5fac13d151a19d3c03cc135c
gayanpurasinghe <gayanpurasinghe.edu@gmail.com>
2026-01-14 23:47:44 +0530

    Introduces a logo asset and CSS for header layout to enhance visual branding and improve the presentation of the ads view.
    Introduces a logo asset and CSS for header layout to enhance
    visual branding and improve the presentation of the ads view.

--- views/ads/view_ads.php
+++ views/ads/view_ads.php
@@ -1,10 +1,18 @@
-<html></html>
+<!DOCTYPE html>
+<html>
 <head>
     <title>View Ads</title>
     <link rel="stylesheet" type="text/css" href="view_ads.css">
 </head>
 <body>
-    <h2>View Ads</h2>
-    <!-- Add your ad display logic here -->
+    
+    <div class="head">
+        <div class="logo">
+            <a href="..\..\views\ads\view_ads.php"><img src="..\..\assets\images\BuySelLogo.png" alt="Home" class="home-icon"></a>
+        </div>
+
+            <h2>View Ads</h2>
+
+    </div>
 </body>
 </html>
