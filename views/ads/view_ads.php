caca6822a34689230a0a029e9f7072a6e5526713
gayanpurasinghe <gayanpurasinghe.edu@gmail.com>
2026-01-15 13:41:15 +0530

    Improves ads page header styling and image assets

--- views/ads/view_ads.php
+++ views/ads/view_ads.php
@@ -13,12 +13,14 @@
             <a href="..\..\views\ads\view_ads.php"><img src="..\..\assets\images\BuySelLogo.png" alt="Home"
                     class="home-icon"></a>
         </div>
+        <div class="name">
+            <a href="..\..\views\ads\view_ads.php"><h1>BuySel.lk</h1></a>
+        </div>
         <div class="nav">
-            <a href="..\..\views\users\view_profile.php">Profile</a>
+            <!--a href="..\..\views\users\view_profile.php">Profile</a-->
             <a href="..\..\views\ads\create_ad.php">Create Ad</a>
-            <a href="..\..\views\ads\view_ads.php" class="active">View Ads</a>
-            <a href="..\..\views\transactions\view_transactions.php">Transactions</a>
-            <a href="..\..\views\users\logout.php">Logout</a>
+            <!--a href="..\..\views\ads\view_ads.php" class="active">View Ads</a-->
+            <a href="..\..\views/auth/login.php">Logout</a>
         </div>
 
     </div>
