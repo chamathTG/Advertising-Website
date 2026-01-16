72ccc3745d80c44734fbcd9020eda0f1629b6aab
gayanpurasinghe <gayanpurasinghe.edu@gmail.com>
2026-01-16 21:30:18 +0530

    Revert "Clearing Code to MVC pattern"
    This reverts commit 86621dc5d200d2395dcec4a1c1e99d0812efaafb.

--- app/views/layout/header.php
+++ app/views/layout/header.php
@@ -3,23 +3,20 @@
 
 <head>
     <title>Header</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/assets/css/layout/header.css">
+    <link rel="stylesheet" type="text/css" href="\dse\C-W\Advertising-Website\public\assets\css\layout\header.css">
 </head>
 
 <body>
-    <?php
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    ?>
+    <?php session_start(); ?>
     <div class="head">
         <div class="logo">
-            <a href="<?php echo URLROOT; ?>"><img src="<?php echo URLROOT; ?>/assets/images/BuySelLogo.png" alt="Home"
+            <a href="\dse\C-W\Advertising-Website\public\index.php"><img
+                    src="\dse\C-W\Advertising-Website\public\assets\images\BuySelLogo.png" alt="Home"
                     class="home-icon"></a>
 
         </div>
         <div class="name">
-            <a href="<?php echo URLROOT; ?>">
+            <a href="\dse\C-W\Advertising-Website\public\index.php">
                 <h1>BuySel.lk</h1>
             </a>
         </div>
@@ -31,9 +28,9 @@
                 <span>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
             <?php endif; ?>
             <!--a href="..\..\views\users\view_profile.php">Profile</a-->
-            <a href="<?php echo URLROOT; ?>/ads/create">Create Ad</a>
+            <a href="\dse\C-W\Advertising-Website\app\views\ads\create_ad.php">Create Ad</a>
             <!--a href="..\..\views\ads\view_ads.php" class="active">View Ads</a-->
-            <a href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+            <a href="\dse\C-W\Advertising-Website\app\views\auth\login.php">Logout</a>
         </div>
 
 
